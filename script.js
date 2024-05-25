be6061aa79fa36a70f96941966d9d019a471a733
Michael T. Daniels <danielssmichael@gmail.com>
2024-05-25 17:08:14 -0400

    replacing javascript file

--- script.js
+++ script.js
@@ -12,4 +12,26 @@ window.addEventListener('scroll', function() {
     }
   });
 
+  $(document).ready(function() {
+    var lastScrollTop = 0;
+    $(window).scroll(function() {
+        var currentScroll = $(this).scrollTop();
+        if (currentScroll > lastScrollTop) {
+            $('.navbar').addClass('navbar-scrolled');
+        } else {
+            $('.navbar').removeClass('navbar-scrolled');
+        }
+        lastScrollTop = currentScroll;
+    });
+});
 
+function toggleNavbar() {
+    var navbarCollapse = document.querySelector('.navbar-collapse');
+    var toggler = document.querySelector('.navbar-toggler');
+    toggler.classList.toggle('change');
+    if (navbarCollapse.style.display === 'block') {
+        navbarCollapse.style.display = 'none';
+    } else {
+        navbarCollapse.style.display = 'block';
+    }
+}
